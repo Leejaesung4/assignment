ca459a847b3004a6f3604d8b18a4f529ddd1aae5
leejaesung <ghtpghdi@gmail.com>
2017-09-28 17:22:04 +0900

    20170928 함수이용 삼각형 별찍기

---
+++ 20170928/20170928.js
@@ -0,0 +1,29 @@
+function triangle(line) {
+  for (var i = 1; i <= line; i++) {
+    var result = '';
+    for (var m = i; m <= line; m++) {
+      result += ' '; //공백=#;
+    }
+    for (var s = 1; s <= i * 2 - 1; s++) {
+      result += '*';
+    }
+    console.log('정 삼각형 : ' + result);
+  };
+};
+triangle(10);
+
+
+//역삼각형
+var reTriangle = function(line) {
+  for (var i = 1; i <= line; i++) {
+    var result = '';
+    for (var m = 1; m <= i; m++) {
+      result += ' '; //공백=#
+    }
+    for (var s = 1; s <= line * 2 - ((i * 2) - 1); s++) {
+      result += '*';
+    }
+    console.log('역 삼각형 : ' + result);
+  }
+};
+reTriangle(10);
